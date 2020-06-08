2e9b090401ee5b9c515d700a6aae0cfb0f881d69
Fadhriga Bestari <fadhriga.bestari@gmail.com>
2020-06-08 09:50:34 +0700

    adjust script to match nuber of jobs tested

--- reset.sh
+++ reset.sh
@@ -3,3 +3,11 @@ kubectl delete tfjob job1
 kubectl delete tfjob job2
 kubectl delete tfjob job3
 kubectl delete tfjob job4
+kubectl delete tfjob job5
+kubectl delete tfjob job6
+kubectl delete tfjob job7
+kubectl delete tfjob job8
+kubectl delete tfjob job9
+kubectl delete tfjob job10
+kubectl delete tfjob job11
+kubectl delete tfjob job12
--- test.sh
+++ test.sh
@@ -3,4 +3,12 @@ kubectl create -f examples/job1.yaml
 kubectl create -f examples/job2.yaml
 kubectl create -f examples/job3.yaml
 kubectl create -f examples/job4.yaml
+kubectl create -f examples/job5.yaml
+kubectl create -f examples/job6.yaml
+kubectl create -f examples/job7.yaml
+kubectl create -f examples/job8.yaml
+kubectl create -f examples/job9.yaml
+kubectl create -f examples/job10.yaml
+kubectl create -f examples/job11.yaml
+kubectl create -f examples/job12.yaml
 kubectl get pods -w
