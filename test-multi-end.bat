98b6e46412bf79d2ca0ec2943a3d6746406e0bf9
Naufal Prima Yoriko <primayoriko@gmail.com>
2022-01-24 22:44:47 +0700

    chore: add multi run example with scaling

---
+++ test-multi-end.bat
@@ -0,0 +1,3 @@
+kubectl delete -f example/multi-1.yaml
+kubectl delete -f example/multi-2.yaml
+kubectl delete -f example/multi-3.yaml
---
+++ test-multi.bat
@@ -0,0 +1,3 @@
+kubectl create -f example/multi-1.yaml
+kubectl create -f example/multi-2.yaml
+@REM kubectl create -f example/multi-3.yaml
