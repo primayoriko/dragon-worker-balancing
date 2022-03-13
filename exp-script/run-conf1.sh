9ae236df5b2ae1a413664977164d10525749294a
Naufal Prima Yoriko <primayoriko@gmail.com>
2022-03-13 13:11:25 +0700

    chore: add script to run experiment

---
+++ exp-script/run-conf1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+kubectl create -f ../example/experiment/conf-1-job1.yaml
+sleep 3
+kubectl create -f ../example/experiment/conf-1-job2.yaml
+sleep 3
+kubectl create -f ../example/experiment/conf-1-job3.yaml
